729ee2f00865ea9daf6928d40df518de63f19cb9
Fabian PETRAS <fpetras@e1r4p3.42.fr>
2018-02-02 16:43:30 +0100

    Libft and header file

---
+++ minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/29 07:39:35 by fpetras           #+#    #+#             */
+/*   Updated: 2018/01/30 09:37:49 by fpetras          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include "libft/get_next_line/get_next_line.h"
+# include "libft/ft_printf/ft_printf.h"
+
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <dirent.h>
+# include <sys/param.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+
+
+#endif
